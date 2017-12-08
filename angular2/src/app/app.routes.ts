c2468e8ab0435ee48614889da48aebdc1d4ed0ef
tuanhnt1712 <tuanhnt1712@gmail.com>
2017-12-08 16:43:14 +0700

    template angular login sign up

--- angular2/src/app/app.routes.ts
+++ angular2/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './user/login.component';
+import { SignupComponent } from './user/signup.component';
 
 const routing : Routes = [
   {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent}
 ]
 
 export const appRoutes = RouterModule.forRoot(routing);
---
+++ angular2/src/app/user/signup.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+export class SignupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
