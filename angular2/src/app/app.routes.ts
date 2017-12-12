62afcf63b7ca0bf4719ff42d6a0d14f99f43ab05
tuanhnt1712 <tuanhnt1712@gmail.com>
2017-12-12 17:56:23 +0700

    angular convert template deck

--- angular2/src/app/app.routes.ts
+++ angular2/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './user/login.component';
 import { SignupComponent } from './user/signup.component';
+import { DeckComponent } from './deck/deck.component';
 
 const routing : Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'signup', component: SignupComponent}
+  {path: 'signup', component: SignupComponent},
+  {path: 'deck', component: DeckComponent}
 ]
 
 export const appRoutes = RouterModule.forRoot(routing);
