e956f647896ef12642ab8def83d9aa976a50b7b9
tuanhnt1712 <tuanhnt1712@gmail.com>
2018-02-02 19:32:51 +0900

    hasdfjkd

--- angular2/src/app/services/authentication.service.ts
+++ angular2/src/app/services/authentication.service.ts
@@ -18,13 +18,15 @@ export class AuthenticationService {
       .map(response => {
           var user = response.data
           if (user && user.token) {
-              localStorage.setItem('currentUser', JSON.stringify(user));
+            this.token = user.token;
+            localStorage.setItem('currentUser', JSON.stringify(user));
           }
           return user;
       });
   }
 
   logout() {
-      localStorage.removeItem('currentUser');
+    this.token = null;
+    localStorage.removeItem('currentUser');
   }
 }
