e5852d0f62f36c7a1029f015ecb9038c9f258dcf
tuanhnt1712 <tuanhnt1712@gmail.com>
2018-02-07 16:58:11 +0700

    refresh token

--- angular2/src/app/services/decks.service.ts
+++ angular2/src/app/services/decks.service.ts
@@ -1,27 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, RequestOptions, Headers } from '@angular/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { AuthenticationService } from '../services/authentication.service';
 import { Deck } from '../deck';
 
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
 import { ActivatedRoute, Router } from '@angular/router';
+import { HeaderBasicService } from './base/header-basic.service';
 
 @Injectable()
-export class DecksService{
+export class DecksService {
   private baseUrl: string = 'http://localhost:3000/api';
 
-  constructor(private http : Http,
+  constructor(private http: Http,
               private authenticationService: AuthenticationService,
               private router: Router
   ){}
-
-  private getHeaders(){
-    let headers = new Headers();
-    headers.append('Accept', 'application/json');
-    headers.append('Doorkeeper-Token', 'Bearer ' + this.getAccess())
-    return headers;
-  }
 
   private getAccess(){
     if (JSON.parse(localStorage.getItem('currentUser')) == null)
@@ -30,22 +26,26 @@ export class DecksService{
       return JSON.parse(localStorage.getItem('currentUser')).token
   }
 
-  getAll(): Observable<Deck[]>{
-    let decks$ = this.http
-      .get(`${this.baseUrl}/decks`, { headers: this.getHeaders()})
-      .map(mapDecks);
-      return decks$;
+  getAll(): Promise<Deck[]>{
+    let decks = this.http
+      .get(`${this.baseUrl}/decks`, { headers: this.authenticationService.getHeaders()})
+      .toPromise()
+      .then(response => {
+        return response.json().data['decks']
+      })
+      .catch(error => this.authenticationService.handleError(error));
+    return decks;
   }
 
   get(id: number): Observable<Deck> {
     let deck$ = this.http
-      .get(`${this.baseUrl}/decks/${id}`, {headers: this.getHeaders()})
+      .get(`${this.baseUrl}/decks/${id}`, {headers: this.authenticationService.getHeaders()})
       .map(mapDeck);
     return deck$;
   }
 
   add_deck(deck) {
-    let headers = this.getHeaders();
+    let headers = this.authenticationService.getHeaders();
     let options = new RequestOptions({ headers: headers });
     let body = deck;
     return this.http.post(`${this.baseUrl}/decks/`, body, options ).map((res: Response) => res.json());
