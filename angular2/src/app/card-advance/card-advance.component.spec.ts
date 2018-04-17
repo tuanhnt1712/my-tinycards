fd95d2c76dd4bb8b1bbc0967ee84615464a377c5
Tu Anh <tuanhnt1712@gmail.com>
2018-04-17 15:52:09 +0700

    Add shortcut behaviors & fix tinymce

--- angular2/src/app/card-advance/card-advance.component.spec.ts
+++ angular2/src/app/card-advance/card-advance.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CardComponent } from './card.component';
+import { CardAdvanceComponent } from './card-advance.component';
 
-describe('CardComponent', () => {
-  let component: CardComponent;
-  let fixture: ComponentFixture<CardComponent>;
+describe('CardAdvanceComponent', () => {
+  let component: CardAdvanceComponent;
+  let fixture: ComponentFixture<CardAdvanceComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CardComponent ]
+      declarations: [ CardAdvanceComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CardComponent);
+    fixture = TestBed.createComponent(CardAdvanceComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
